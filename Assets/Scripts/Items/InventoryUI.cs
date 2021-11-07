dc2dc705f0a8aee8f63d722cdfd51efa02803f55
Arugaf <arug113@gmail.com>
2021-11-07 15:05:03 +0300

    added inventory

---
+++ Assets/Scripts/Items/InventoryUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Items {
+    public class InventoryUI : MonoBehaviour {
+        [SerializeField] private List<Image> items;
+
+        private int _index = 0;
+
+        public void ChangeItem(Sprite image) {
+            items[_index++].sprite = image;
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerInteractor.cs
+++ Assets/Scripts/Player/PlayerInteractor.cs
@@ -1,12 +1,34 @@
-using System;
 using Items;
 using UnityEngine;
 
 namespace Player {
     public class PlayerInteractor : MonoBehaviour {
-        private InteractableItem _item = null;
+        private InteractableItem _item;
+        private PlayerInventory _inventory;
+
+        private Crime _crimeScene;
+
+        public void Start() {
+            _inventory = FindObjectOfType<PlayerInventory>();
+            _crimeScene = FindObjectOfType<Crime>();
+
+            if (_inventory == null) {
+                Debug.Log("oh no");
+            }
+        }
+
         public void Update() {
-            if (Input.GetKeyDown("e") && _item) {
+            if (Input.GetKeyDown("tab") && _inventory.IsFull()) {
+                _crimeScene.ChangeState();
+            }
+            
+            if (!Input.GetKeyDown("e") || !_item) {
+                return;
+            }
+
+            var item = _item.GetComponent<Item>();
+            if (_inventory.AddItem(item.itemName, item)) {
+                _crimeScene.inventory.ChangeItem(item.itemSprite);
                 _item.DestroyItem();
             }
         }
