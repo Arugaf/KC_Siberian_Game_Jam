a12b6ce6816058954d89e3ae67c85aa171082a1a
Arugaf <arug113@gmail.com>
2021-11-07 15:05:31 +0300

    crime scene pre-release

--- Assets/Scripts/Ending.cs
+++ Assets/Scripts/Ending.cs
@@ -1,14 +1,19 @@
+using Player;
 using UnityEngine;
 
 public class Ending : MonoBehaviour {
     private G _g;
 
+    private PlayerInventory _inventory;
+
     public void Start() {
         _g = FindObjectOfType<G>();
+        _inventory = FindObjectOfType<PlayerInventory>();
     }
 
     public void ChangeState() {
         _g.CurrentState = GameState.Menu;
+        _inventory.Reset();
         _g.sceneController.SwitchScene(_g.CurrentState);
     }
 }
