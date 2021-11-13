272b65b684e240cb46fcde5ef4a1d8a879bb87f0
dikopr0s@gmail.com <dikopr0s@gmail.com>
2021-11-13 18:34:54 +0300

    fix standalone build/cant fix webGL

--- Assets/Scripts/JsonLoader.cs
+++ Assets/Scripts/JsonLoader.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using Newtonsoft.Json;
+using UnityEngine.Networking;
 
 static public class JsonLoader
 {
+   static string fileText;
+   static string filePath;
+   static bool isReady;
+   static UnityWebRequest www;
    static public void LoadInfoFromFile<T>(string fileName, ref List<T> container) {
-        string filePath = Application.dataPath + "/Scripts/" + fileName + ".json";
-
-        if(File.Exists(filePath))
+        #if UNITY_WEBGL
+            filePath = Path.Combine(Application.streamingAssetsPath, "/StreamingAssets" + fileName);
+            www = UnityWebRequest.Get(filePath);
+            Debug.Log("try get file - " + filePath);
+            StartCoroutine(LoadFile(filePath));
+            container = JsonConvert.DeserializeObject<List<T>>(fileText);
+        #else
+            filePath = Application.streamingAssetsPath + fileName;//Application.dataPath + "/Scripts/" + fileName + ".json";
+            if(!File.Exists(filePath))
+                Debug.LogError("File - " + filePath + " doesn't exist");
             container = JsonConvert.DeserializeObject<List<T>>(File.ReadAllText(filePath));
-        else
-            Debug.LogError("File - " + filePath + " doesn't exist");
+        #endif
+    }
+
+    static IEnumerator LoadFile(string _s) {
+        Debug.Log("Coroutine started in - " + _s);
+        yield return www.SendWebRequest();
+        Debug.LogWarning("www - " + www.result);
+        fileText = www.downloadHandler.text;
+        if(www.downloadHandler.isDone)
+            isReady = www.downloadHandler.isDone;
+        Debug.Log("Load from server - " + www.downloadHandler.text);
     }
 }
